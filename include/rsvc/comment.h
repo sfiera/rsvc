cfddc62b31124240623cab21bf9e8f5e151310d1
Chris Pickel <sfiera@sfzmail.com>
2012-01-23 09:05:57 -0500

    Add docs to comment.h.

--- include/rsvc/comment.h
+++ include/rsvc/comment.h
@@ -27,29 +27,56 @@
 
 typedef struct rsvc_comments* rsvc_comments_t;
 
-#define RSVC_TITLE      "TITLE"
-#define RSVC_ALBUM      "ALBUM"
-#define RSVC_ARTIST     "ARTIST"
-#define RSVC_GENRE      "GENRE"
-#define RSVC_DATE       "DATE"
-
-#define RSVC_TRACKNUMBER \
-                        "TRACKNUMBER"
-#define RSVC_TRACKTOTAL "TRACKTOTAL"
-#define RSVC_DISCNUMBER "DISCNUMBER"
-#define RSVC_DISCTOTAL  "DISCTOTAL"
-
-#define RSVC_ISRC       "ISRC"
-#define RSVC_MCN        "MCN"
-#define RSVC_ENCODER    "ENCODER"
-
-#define RSVC_MUSICBRAINZ_DISCID \
-                        "MUSICBRAINZ_DISCID"
-
+/// Comments
+/// ========
+///
+/// ..  type:: rsvc_comments_t
+///
+///     Represents a collection of name-value pairs patterned after
+///     Vorbis comments.  Models a one-to-many map of strings to
+///     strings.  The names may have any uppercase ASCII string as their
+///     value, but see :ref:`comments_constants` for well-known values.
+///
+///     :type:`rsvc_comments_t` objects are created with
+///     :func:`rsvc_comments_create()` or :func:`rsvc_comments_copy()`
+///     and destroyed with :func:`rsvc_comments_destroy()`.
+///
+/// ..  function:: rsvc_comments_t rsvc_comments_create()
+///
+///     :returns:       An empty set of comments.
+///
+/// ..  function:: rsvc_comments_t rsvc_comments_copy(rsvc_comments_t comments)
+///
+///     :returns:       A copy of `comments`.
+///
+/// ..  function:: void rsvc_comments_destroy(rsvc_comments_t comments)
+///
+///     Destroys a :type:`rsvc_comments_t`, reclaiming its resources.
 rsvc_comments_t         rsvc_comments_create();
 rsvc_comments_t         rsvc_comments_copy(rsvc_comments_t comments);
 void                    rsvc_comments_destroy(rsvc_comments_t comments);
 
+/// ..  function:: void rsvc_comments_clear(rsvc_comments_t comments, const char* name)
+///
+///     Removes all comments with name `name`.
+///
+/// ..  function:: void rsvc_comments_add(rsvc_comments_t comments,
+///                                       const char* name, const char* value)
+///
+///     Adds a comment with name `name` and value `value`.  Does not
+///     modify or overwrite any existing comment with name `name`.
+///
+/// ..  function:: void rsvc_comments_add_int(rsvc_comments_t comments,
+///                                           const char* name, int value)
+///
+///     Calls :func:`rsvc_comments_add()` with a stringified form of
+///     `value`.
+///
+/// ..  function:: void rsvc_comments_set(rsvc_comments_t comments,
+///                                       const char* name, const char* value)
+///
+///     Removes any existing comments with name `name` and adds a new
+///     one with name `name` and name `value`.
 void                    rsvc_comments_clear(rsvc_comments_t comments, const char* name);
 void                    rsvc_comments_add(rsvc_comments_t comments,
                                           const char* name, const char* value);
@@ -58,6 +85,48 @@ void                    rsvc_comments_add_int(rsvc_comments_t comments,
 void                    rsvc_comments_set(rsvc_comments_t comments,
                                           const char* name, const char* value);
 
+/// ..  function:: size_t rsvc_comments_size(rsvc_comments_t comments)
+///
+///     :returns:       The total number of comments in `comments`.
+///
+/// ..  function:: bool rsvc_comments_get(rsvc_comments_t comments, const char* names[],
+///                                       const char* values[], size_t* ncomments)
+///
+///     Attempts to place all comments into the arrays `names` and
+///     `values`.  If there is enough space to read all comments into
+///     the arrays, reads all of them and returns true; otherwise,
+///     reads as many as possible then returns false.
+///
+///     :param names:       An array of size at least ``*ncomments``.
+///     :param values:      An array of size at least ``*ncomments``.
+///     :param ncomments:   Initially, the size of `names` and `values`;
+///                         on return, the number of comments read.
+///     :returns:           true iff all comments were read.
+///
+/// ..  function:: size_t rsvc_comments_count(rsvc_comments_t comments, const char* name)
+///
+///     :param name:    A comment name.
+///     :returns:       The number of comments with name `name`.
+///
+/// ..  function:: bool rsvc_comments_find(rsvc_comments_t comments, const char* name,
+///                                        const char* values[], size_t* nvalues)
+///
+///     Attempts to read all values of the comment `name` into `values`.
+///     If there is enough space to read all comments into the arrays,
+///     reads all of them and returns true; otherwise, reads as many as
+///     possible then returns false.
+///
+///     :param name:        A comment name.
+///     :param values:      An array of size at least ``*nvalues``.
+///     :param nvalues:     Initially, the size of `values`; on return,
+///                         the number of comments read.
+///     :returns:           true iff all comments were read.
+///
+/// ..  function:: bool rsvc_comments_each(rsvc_comments_t comments,
+///                                        void (^block)(const char*, const char*, rsvc_stop_t))
+///
+///     Iterates over comments in `comments`.  See :type:`rsvc_stop_t`
+///     for a description of the iterator interface.
 size_t                  rsvc_comments_size(rsvc_comments_t comments);
 bool                    rsvc_comments_get(rsvc_comments_t comments, const char* names[],
                                           const char* values[], size_t* ncomments);
@@ -66,5 +135,40 @@ bool                    rsvc_comments_find(rsvc_comments_t comments, const char*
                                            const char* values[], size_t* nvalues);
 bool                    rsvc_comments_each(rsvc_comments_t comments,
                                            void (^block)(const char*, const char*, rsvc_stop_t));
+
+/// ..  _comments_constants:
+///
+/// Constants
+/// ---------
+///
+/// ..  var:: RSVC_TITLE
+/// ..  var:: RSVC_ALBUM
+/// ..  var:: RSVC_ARTIST
+/// ..  var:: RSVC_GENRE
+/// ..  var:: RSVC_DATE
+#define RSVC_TITLE                  "TITLE"
+#define RSVC_ALBUM                  "ALBUM"
+#define RSVC_ARTIST                 "ARTIST"
+#define RSVC_GENRE                  "GENRE"
+#define RSVC_DATE                   "DATE"
+
+/// ..  var:: RSVC_TRACKNUMBER
+/// ..  var:: RSVC_TRACKTOTAL
+/// ..  var:: RSVC_DISCNUMBER
+/// ..  var:: RSVC_DISCTOTAL
+#define RSVC_TRACKNUMBER            "TRACKNUMBER"
+#define RSVC_TRACKTOTAL             "TRACKTOTAL"
+#define RSVC_DISCNUMBER             "DISCNUMBER"
+#define RSVC_DISCTOTAL              "DISCTOTAL"
+
+/// ..  var:: RSVC_ISRC
+/// ..  var:: RSVC_MCN
+/// ..  var:: RSVC_ENCODER
+#define RSVC_ISRC                   "ISRC"
+#define RSVC_MCN                    "MCN"
+#define RSVC_ENCODER                "ENCODER"
+
+/// ..  var:: RSVC_MUSICBRAINZ_DISCID
+#define RSVC_MUSICBRAINZ_DISCID     "MUSICBRAINZ_DISCID"
 
 #endif  // RSVC_COMMENT_H_
