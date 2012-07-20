b0d06f0370f7cec58d2c893aee19702d1333ecac
Chris Pickel <sfiera@sfzmail.com>
2012-07-20 16:52:40 -0400

    Explicitly track segments in renames.

--- src/bin/cloak.c
+++ src/bin/cloak.c
@@ -693,7 +693,13 @@ static int ops_mode(ops_t ops) {
     }
 }
 
-static void _segment(const char* path, size_t** segments, size_t* nsegments) {
+typedef struct segment* segment_t;
+struct segment {
+    const char* data;
+    size_t size;
+};
+
+static void segment_noalloc(const char* path, segment_t* segments, size_t* nsegments) {
     size_t i = 0;
     while (*path) {
         size_t span = strcspn(path, " ./");
@@ -701,7 +707,8 @@ static void _segment(const char* path, size_t** segments, size_t* nsegments) {
             span = 1;
         }
         if (segments) {
-            (*segments)[i] = span;
+            (*segments)[i].data = path;
+            (*segments)[i].size = span;
         }
         ++i;
         path += span;
@@ -711,48 +718,35 @@ static void _segment(const char* path, size_t** segments, size_t* nsegments) {
     }
 }
 
-static void segment(const char* path, size_t** segments, size_t* nsegments) {
-    _segment(path, NULL, nsegments);
-    *segments = malloc(*nsegments * sizeof(size_t));
-    _segment(path, segments, NULL);
+static void segment(const char* path, segment_t* segments, size_t* nsegments) {
+    segment_noalloc(path, NULL, nsegments);
+    *segments = malloc(*nsegments * sizeof(struct segment));
+    segment_noalloc(path, segments, NULL);
 }
 
 static bool _find_next_common_segment(
-        const char* a, size_t* aseg, size_t a_equal,
-        const char* b, size_t* bseg, size_t nbseg, size_t* b_equal) {
-    for (size_t i = 0; i < a_equal; ++i) {
-        a += aseg[i];
-    }
-    for (size_t i = 0; i <= a_equal; ++i) {
-        if ((aseg[a_equal] == bseg[i]) && (strncmp(a, b, aseg[a_equal]) == 0)) {
-            if (strchr(" ./", *a) == NULL) {
+        segment_t aseg, size_t a_equal,
+        segment_t bseg, size_t nbseg, size_t* b_equal) {
+    for (size_t i = 0; (i <= a_equal) && (i <= nbseg); ++i) {
+        if ((aseg[a_equal].size == bseg[i].size)
+                && (strncmp(aseg[a_equal].data, bseg[i].data, aseg[a_equal].size) == 0)) {
+            if (strchr(" ./", *(aseg[a_equal].data)) == NULL) {
                 *b_equal = i;
                 return true;
             }
         }
-        b += bseg[i];
     }
     return false;
 }
 
 static void backtrack_single_segments(
-        const char* src, size_t* srcseg, size_t* src_equal,
-        const char* dst, size_t* dstseg, size_t* dst_equal) {
-    for (size_t i = 0; i < *src_equal; ++i) {
-        src += srcseg[i];
-    }
-    for (size_t i = 0; i < *dst_equal; ++i) {
-        dst += dstseg[i];
-    }
+        segment_t srcseg, size_t* src_equal,
+        segment_t dstseg, size_t* dst_equal) {
     while ((*src_equal > 0) && (*dst_equal > 0)) {
-        size_t srcsize = srcseg[(*src_equal) - 1];
-        size_t dstsize = dstseg[(*dst_equal) - 1];
-        if ((srcsize != dstsize) || (srcsize != 1)) {
-            return;
-        }
-        --src;
-        --dst;
-        if (*src != *dst) {
+        segment_t src = srcseg + (*src_equal) - 1;
+        segment_t dst = dstseg + (*dst_equal) - 1;
+        if ((src->size != dst->size) || (src->size != 1)
+                || (*(src->data) != *(dst->data))) {
             return;
         }
         --(*src_equal);
@@ -761,23 +755,21 @@ static void backtrack_single_segments(
 }
 
 static void find_next_common_segment(
-        const char* src, size_t* srcseg, size_t nsrcseg, size_t* src_equal,
-        const char* dst, size_t* dstseg, size_t ndstseg, size_t* dst_equal) {
-    *src_equal = nsrcseg;
-    *dst_equal = ndstseg;
+        segment_t srcseg, size_t nsrcseg, size_t* src_equal,
+        segment_t dstseg, size_t ndstseg, size_t* dst_equal) {
     size_t maxseg = (nsrcseg > ndstseg) ? nsrcseg : ndstseg;
     for (size_t i = 1; i < maxseg; ++i) {
         size_t k;
-        if ((i < nsrcseg) && _find_next_common_segment(src, srcseg, i, dst, dstseg, ndstseg, &k)) {
+        if ((i < nsrcseg) && _find_next_common_segment(srcseg, i, dstseg, ndstseg, &k)) {
             *src_equal = i;
             *dst_equal = k;
-            backtrack_single_segments(src, srcseg, src_equal, dst, dstseg, dst_equal);
+            backtrack_single_segments(srcseg, src_equal, dstseg, dst_equal);
             return;
         }
-        if ((i < ndstseg) && _find_next_common_segment(dst, dstseg, i, src, srcseg, nsrcseg, &k)) {
+        if ((i < ndstseg) && _find_next_common_segment(dstseg, i, srcseg, nsrcseg, &k)) {
             *src_equal = k;
             *dst_equal = i;
-            backtrack_single_segments(src, srcseg, src_equal, dst, dstseg, dst_equal);
+            backtrack_single_segments(srcseg, src_equal, dstseg, dst_equal);
             return;
         }
     }
@@ -785,49 +777,49 @@ static void find_next_common_segment(
     *dst_equal = ndstseg;
 }
 
-static void skip_advance_segment(const char** str, size_t** seg, size_t* nseg) {
-    *str += **seg;
+static void skip_advance_segment(segment_t* seg, size_t* nseg) {
     ++(*seg);
     --(*nseg);
 }
 
-static void print_advance_segment(const char** str, size_t** seg, size_t* nseg) {
-    char* copy = strndup(*str, **seg);
+static void print_advance_segment(segment_t* seg, size_t* nseg) {
+    char* copy = strndup((*seg)->data, (*seg)->size);
     printf("%s", copy);
     free(copy);
-    skip_advance_segment(str, seg, nseg);
+    skip_advance_segment(seg, nseg);
 }
 
 static void print_rename(const char* src, const char* dst) {
-    size_t* srcseg;
+    segment_t srcseg;
     size_t nsrcseg;
     segment(src, &srcseg, &nsrcseg);
-    size_t* dstseg;
+    segment_t dstseg;
     size_t ndstseg;
     segment(dst, &dstseg, &ndstseg);
-    size_t* srcseg_save = srcseg;
-    size_t* dstseg_save = dstseg;
+    segment_t srcseg_save = srcseg;
+    segment_t dstseg_save = dstseg;
 
     printf("rename: ");
     while (nsrcseg && ndstseg) {
         // Segments are equal.
-        if ((*srcseg == *dstseg) && (strncmp(src, dst, *srcseg) == 0)) {
-            print_advance_segment(&src, &srcseg, &nsrcseg);
-            skip_advance_segment(&dst, &dstseg, &ndstseg);
+        if ((srcseg->size == dstseg->size)
+                && (strncmp(srcseg->data, dstseg->data, srcseg->size) == 0)) {
+            print_advance_segment(&srcseg, &nsrcseg);
+            skip_advance_segment(&dstseg, &ndstseg);
             continue;
         }
 
         // Segments are not equal.
         size_t src_equal, dst_equal;
-        find_next_common_segment(src, srcseg, nsrcseg, &src_equal,
-                                 dst, dstseg, ndstseg, &dst_equal);
+        find_next_common_segment(srcseg, nsrcseg, &src_equal,
+                                 dstseg, ndstseg, &dst_equal);
         printf("{");
         for (size_t i = 0; i < src_equal; ++i) {
-            print_advance_segment(&src, &srcseg, &nsrcseg);
+            print_advance_segment(&srcseg, &nsrcseg);
         }
         printf(" => ");
         for (size_t i = 0; i < dst_equal; ++i) {
-            print_advance_segment(&dst, &dstseg, &ndstseg);
+            print_advance_segment(&dstseg, &ndstseg);
         }
         printf("}");
     }
