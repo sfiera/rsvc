d4268a2e2e28c75c0b0bbcd0aed447bc987e7857
Chris Pickel <sfiera@sfzmail.com>
2014-12-19 19:50:16 +0900

    Tell rsvc about png/jpeg to use them in images.

--- src/bin/rsvc.c
+++ src/bin/rsvc.c
@@ -32,9 +32,11 @@
 #include <rsvc/disc.h>
 #include <rsvc/flac.h>
 #include <rsvc/id3.h>
+#include <rsvc/jpeg.h>
 #include <rsvc/lame.h>
 #include <rsvc/mad.h>
 #include <rsvc/mp4.h>
+#include <rsvc/png.h>
 #include <rsvc/vorbis.h>
 
 const char*                 rsvc_progname;
@@ -62,6 +64,9 @@ static void rsvc_main(int argc, char* const* argv) {
     rsvc_mad_format_register();
     rsvc_mp4_format_register();
     rsvc_vorbis_format_register();
+
+    rsvc_jpeg_format_register();
+    rsvc_png_format_register();
 
     rsvc_progname = strdup(basename(argv[0]));
 
